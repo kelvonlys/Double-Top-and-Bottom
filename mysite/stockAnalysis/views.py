d23e4e1293879b9d6fbc2ac3b6def5db0eb47ab0
Kelvin Lai <kelvinlys950227@hotmail.com>
2018-03-03 16:46:59 +0800

    successfully detect double bottom

--- mysite/stockAnalysis/views.py
+++ mysite/stockAnalysis/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect #you should delete this later on
 from .forms import NameForm
 
+
 import pandas as pd
 import quandl
 import json
+from math import log10, floor
+
 
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
@@ -31,6 +34,11 @@ result = ""
 highest = 0
 lowest = 0
 rsiPrice = 0
+spread_price = 0
+
+df = quandl.get("HKEX/"+"00939", returns="numpy", rows=30, authtoken=API)
+test = 'Low'
+print("testing: ", df[test])
 
 def index(request):
     if request.method == 'POST':
@@ -48,33 +56,18 @@ def index(request):
 def getStockInfo (num): 
     global highest 
     global lowest 
-    df = quandl.get("HKEX/"+num, returns="numpy", rows=30, authtoken=API)
+    df = quandl.get("HKEX/"+num, returns="numpy", rows=50, authtoken=API)
     priceHigh = df['High']
     priceLow = df['Low']
     nominal = df['Nominal Price']
     rsiPrice = df['Previous Close']
+    global spread_price
+    spread_price = find_spread(df['Previous Close'][df.size-1])
     #calMax(df)
     calMin(df)
     highest = 0
     lowest = min(priceLow)
     
-
-def calMin(obj):
-    minPrices = numpy.array(obj['Low'])
-    minPrices = 1./minPrices
-    indexes = peakutils.indexes(minPrices, thres=0.02/max(minPrices), min_dist=1) #you can fine tune the thres to smaller value to get even shorter period
-    print("min: ", indexes)
-
-    for i in range (0, indexes.size):
-        print("print i: ",i)
-        print("lowest: ", obj[indexes[i]]['Date'])
-        print("lowest price: ", obj[indexes[i]]['Low'])
-        for j in range (i + 1, indexes.size):
-            if obj[indexes[i]]['Low'] == obj[indexes[j]]['Low']:
-                print("indexes[i] ", obj[indexes[i]]['Low'])
-
-        
-
 
 def calMax(obj):
     maxPrices = numpy.array(obj['High'])
@@ -86,6 +79,55 @@ def calMax(obj):
     for y in range(indexes.size):
         print("prices:", obj[indexes[y-1]]['High'])
         print("prices: ", obj[indexes[y-1]])
+
+def calMin(obj):
+    minPrices = numpy.array(obj['Low'])
+    minPrices = 1./minPrices
+    indexes = peakutils.indexes(minPrices, thres=0.02/max(minPrices), min_dist=1) #you can fine tune the thres to smaller value to get even shorter period
+    print("min: ", indexes)
+    for i in range (0, indexes.size):
+        #print("lowest: ", obj[indexes[i]]['Date'])
+        for j in range (i + 1, indexes.size):
+            num1 = obj[indexes[i]]['Low']
+            num2 = obj[indexes[j]]['Low']
+            date1 = obj[indexes[i]]['Date']
+            date2 = obj[indexes[j]]['Date']
+            volume1 = obj[indexes[i]]['Share Volume (000)']
+            volume2 = obj[indexes[j]]['Share Volume (000)']
+            #print("diff: ", round(abs(num1-num2), -int(floor(log10(abs(num1-num2))))))
+            diff = round(abs(num1-num2), -int(floor(log10(abs(num1-num2)))))
+            if (diff <= spread_price*10):
+                print("double bottom captured, if you see the next message, volume passed too :D")
+                if  (volume1 > volume2): 
+                    print("double bottom captured, info as below: ")
+                    print("i: ", num1, " i's volume: ", volume1, "date: ", date1)
+                    print("j: ", num2, 'i volume: ', volume2, "date: ", date2)
+                    print(spread_price*10)
+
+
+        
+def find_spread(self):
+    #Spread table from HKEX
+    if (0.01 <= self <= 0.25):
+        return float(0.001)
+    elif  (0.25 < self <= 0.5):
+        return float(0.005)
+    elif (0.50 < self <= 10.00):
+        return float(0.010)
+    elif (10.00 < self <= 20.00):
+        return float(0.020)
+    elif (20.00 < self <= 100.00):
+        return float(0.050)
+    elif (100.00 < self <= 200.00):
+        return float(0.100)
+    elif (200.00 < self <= 500.00):
+        return float(0.200)
+    elif (1000.00 < self <= 2000.00):
+        return float(1.000)
+    elif (2000.00 < self <= 5000.00):
+        return float(2.000)
+    else:
+        return float(5.000)
 
 '''
 dfReset = df.reset_index()
