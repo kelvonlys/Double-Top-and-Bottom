fc0de45c18da9757651a02c71b5d1637314b7719
Kelvin Lai <kelvinlys950227@hotmail.com>
2018-03-05 22:15:17 +0800

    applied another library detect_peaks and replace the peakutil. Also, the case of the absence of double top and bottom is dealt with, by just looking for the double top or bottom without the volume

--- mysite/stockAnalysis/views.py
+++ mysite/stockAnalysis/views.py
@@ -19,7 +19,7 @@ import talib
 
 ############# for min/max detection ###########
 import peakutils
-from scipy import signal
+from detect_peaks import detect_peaks
 
 close = numpy.random.random(100)
 
@@ -31,25 +31,13 @@ logging.basicConfig(level=logging.INFO)
 API = "z3xWGdrRdxGC1sFtyimA"
 
 result = ""
-highest = 0
-lowest = 0
+buy_price = 0
+sell_price = 0
 rsiPrice = 0
 spread_price = 0
+double_top = "No"
+double_bottom = "No"
 
-df = quandl.get("HKEX/"+"01398", start_date="2017-08-20", end_date="2018-03-01", authtoken=API)
-dfReset = df.reset_index()
-dfRename = pd.DataFrame(dfReset, columns=['Date', 'High'])
-dfRename = dfRename.dropna(how='any')
-
-dfRename = dfRename.to_records(index=False)
-print("df: ", dfRename['High'])
-
-df2 = quandl.get("HKEX/"+"01398", returns="numpy", start_date="2017-08-20", end_date="2018-03-01", authtoken=API)
-#print("df2: ", df2)
-#maxPrices = numpy.array(dfRename['High'])
-#print("maxPrice: ", maxPrices)
-#indexes = peakutils.indexes(maxPrices, thres=0.02/max(maxPrices), min_dist=1) #you can fine tune the thres to smaller value to get even shorter period
-#print("max/min: ", indexes)
 
 
 def index(request):
@@ -60,65 +48,103 @@ def index(request):
             result = form.cleaned_data['stockNum']
             getStockInfo (result)
             #getRSI (result)
-            return render(request, 'stockAnalysis/basic.html', {'price': [result, highest, lowest]})
+            return render(request, 'stockAnalysis/basic.html', {'price': [result, buy_price, sell_price, double_top, double_bottom]})
     else:
         form = NameForm()
     return render(request, 'stockAnalysis/home.html', {'form': form})
 
 def getStockInfo (num): 
-    global highest 
-    global lowest 
-    df = quandl.get("HKEX/"+num, returns="numpy", start_date="2017-08-20", end_date="2018-03-01", authtoken=API)
-    df = df[~numpy.isnan(df)]
-    priceHigh = df['High']
-    priceLow = df['Low']
-    print("priceLow: ",priceLow)
-    nominal = df['Nominal Price']
-    rsiPrice = df['Previous Close']
     global spread_price
-    spread_price = find_spread(df['Previous Close'][df.size-1])
-    #calMax(df)
-    calMin(df)
-    highest = 0
-    lowest = min(priceLow)
-    
+    df = quandl.get("HKEX/"+num, start_date="2018-01-01", end_date="2018-03-04", authtoken=API)
+    df_reset = df.reset_index()
+    df_reframe = pd.DataFrame(df_reset, columns=['Date', 'High','Low', 'Share Volume (000)','Previous Close'])
+    df_reframe = df_reframe.dropna(how='any')
+    df_reframe = df_reframe.to_records(index=False)
+    rsiPrice = df['Previous Close']
+    spread_price = find_spread(df_reframe['Previous Close'][0])
+    find_pattern(df_reframe, "Low")
+    find_pattern(df_reframe, "High")
 
-def calMax(obj):
-    maxPrices = numpy.array(obj['High'])
-    print("maxPrice: ", maxPrices)
-    indexes = peakutils.indexes(maxPrices, thres=0.02/max(maxPrices), min_dist=1) #you can fine tune the thres to smaller value to get even shorter period
-    print("max/min: ", indexes)
-    x = indexes.size
-    print("x: ",x)
-    for y in range(indexes.size):
-        print("prices:", obj[indexes[y-1]]['High'])
-        print("prices: ", obj[indexes[y-1]])
 
-def calMin(obj):
-    minPrices = numpy.array(obj['Low'])
-    print("minPrices: ",minPrices)
-    minPrices = 1./minPrices
-    
-    #indexes = peakutils.indexes(minPrices, thres=0.02/max(maxPrices), min_dist=1) #you can fine tune the thres to smaller value to get even shorter period
-    '''print("min: ", indexes)
+def find_pattern(obj, x):
+    spread_vector = 5
+    global buy_price
+    global sell_price
+    global double_top
+    global double_bottom
+    price = numpy.array(obj[x])
+    if (x == "Low"):
+        indexes = detect_peaks(price, threshold=0.02/max(price), mpd=1, valley=True)
+    else:
+        indexes = detect_peaks(price, threshold=0.02/max(price), mpd=1) #you can fine tune the thres to smaller value to get even shorter period
+    print("index: ", indexes)
     for i in range (0, indexes.size):
-        print("lowest: ", obj[indexes[i]]['Date'])
         for j in range (i + 1, indexes.size):
-            num1 = obj[indexes[i]]['Low']
-            num2 = obj[indexes[j]]['Low']
+            num1 = obj[indexes[i]][x]
+            num2 = obj[indexes[j]][x]
             date1 = obj[indexes[i]]['Date']
             date2 = obj[indexes[j]]['Date']
             volume1 = obj[indexes[i]]['Share Volume (000)']
             volume2 = obj[indexes[j]]['Share Volume (000)']
-            #print("diff: ", round(abs(num1-num2), -int(floor(log10(abs(num1-num2))))))
-            diff = round(abs(num1-num2), -int(floor(log10(abs(num1-num2)))))
-            if (diff <= spread_price*10):
-                print("double bottom captured, if you see the next message, volume passed too :D")
-                if  (volume1 > volume2): 
-                    print("double bottom captured, info as below: ")
-                    print("i: ", num1, " i's volume: ", volume1, "date: ", date1)
-                    print("j: ", num2, 'i volume: ', volume2, "date: ", date2)
-                    print(spread_price*10)'''
+            #print("i: ", num1, " i's volume: ", volume1, "date: ", date1)
+            #print("j: ", num2, 'i volume: ', volume2, "date: ", date2)
+            if (num1-num2)!= 0: #to handle the case log10(0) which would result in math error 
+                diff = round(abs(num1-num2), -int(floor(log10(abs(num1-num2)))))
+            else:
+                diff = abs(num1-num2)
+            if (x == "Low"):
+                if (diff <= spread_price*spread_vector):
+                    print("double bottom captured, if you see the next message, volume passed too :D")
+                    if  (volume1 > volume2): 
+                        #print("double bottom captured, info as below: ")
+                        #print("i: ", num1, " i's volume: ", volume1, "date: ", date1)
+                        #print("j: ", num2, 'i volume: ', volume2, "date: ", date2)
+                        #print(spread_price*spread_vector)
+                        buy_price = min(num1,num2)
+                        double_bottom = "Yes"
+                else:
+                    for k in range (0, len(obj)):
+                        norm_price = obj[k][x]
+                        adjusted_price = min(num1,num2)
+                        if (adjusted_price-norm_price)!= 0: #to handle the case log10(0) which would result in math error 
+                            diff = round(abs(adjusted_price-norm_price), -int(floor(log10(abs(adjusted_price-norm_price)))))
+                        else:
+                            diff = abs(adjusted_price-norm_price)
+                        if (diff <= spread_price*spread_vector):
+                            if  (volume1 > volume2): 
+                                buy_price = min(adjusted_price,norm_price)
+                                double_top = "Yes"
+                            else:
+                                buy_price = min(adjusted_price,norm_price)
+                                double_top = "Yes but without volume"
+                                print("double top found without volume")
+                        else:
+                            buy_price = min(adjusted_price,norm_price)
+            else: # (x == "High")
+                if (diff <= spread_price*spread_vector):
+                    print("double top captured, if you see the next message, volume passed too :D")
+                    if  (volume1 > volume2): 
+                        sell_price = max(num1,num2)
+                        double_top = "Yes"
+                        print("double bottom captured, info as below: ")
+                        print("i: ", num1, " i's volume: ", volume1, "date: ", date1)
+                        print("j: ", num2, 'i volume: ', volume2, "date: ", date2)
+                else:
+                    for k in range (0, len(obj)):
+                        norm_price = obj[k][x]
+                        adjusted_price = max(num1,num2)
+                        if (adjusted_price-norm_price)!= 0: #to handle the case log10(0) which would result in math error 
+                            diff = round(abs(adjusted_price-norm_price), -int(floor(log10(abs(adjusted_price-norm_price)))))
+                        else:
+                            diff = abs(adjusted_price-norm_price)
+                        #print("lowest K: ", num1)
+                        if (diff <= spread_price*spread_vector):
+                            if  (volume1 > volume2): 
+                                sell_price = max(adjusted_price,norm_price)
+                                double_top = "Yes"
+                            else:
+                                sell_price = max(adjusted_price,norm_price)
+                                double_top = "Yes but without volume"
 
 
         
@@ -144,7 +170,7 @@ def find_spread(self):
         return float(2.000)
     else:
         return float(5.000)
-
+getStockInfo("00700")
 '''
 dfReset = df.reset_index()
 dfRename = pd.DataFrame(dfReset, columns=['Date', 'High'])
@@ -166,7 +192,7 @@ def getRSI (num):
     print("RSI: ",talib.RSI(rsiPrice, timeperiod=14))
 
 def doubleTopBottom(request):
-    return render(request, 'stockAnalysis/basic.html',{'origin':[result, highest, lowest]})
+    return render(request, 'stockAnalysis/basic.html',{'origin':[result, buy_price, sell_price]})
 
 
 
