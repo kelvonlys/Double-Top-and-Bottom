fd16d42f5c991991924658307e649c38edcd1da5
kelvonlys <78408180+kelvonlys@users.noreply.github.com>
2021-02-20 13:49:30 +0800

    Delete bin directory

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/laiyukshing/Desktop/fyp_django_practice2/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- bin/pilfont.py
+++
@@ -1,4 +0,0 @@
-#!/Users/laiyukshing/Desktop/fyp/bin/python
-# EASY-INSTALL-SCRIPT: 'Pillow==2.2.1','pilfont.py'
-__requires__ = 'Pillow==2.2.1'
-__import__('pkg_resources').run_script('Pillow==2.2.1', 'pilfont.py')
